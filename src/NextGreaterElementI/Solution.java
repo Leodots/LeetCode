1457f9246857cc84ce706f00f75de427e3366485
leodots <2212.leonardo@gmail.com>
2022-03-17 22:02:12 -0300

    Resolving 496 challenge.

--- src/NextGreaterElementI/Solution.java
+++ src/NextGreaterElementI/Solution.java
@@ -5,10 +5,8 @@ import java.util.*;
 public class Solution {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
         Stack<Integer> stack = new Stack<>();
         Map<Integer, Integer> map = new HashMap<>();
-
         for (int num: nums2) {
 
             while(!stack.isEmpty() && stack.peek() < num) {
@@ -16,21 +14,15 @@ public class Solution {
             }
             stack.push(num);
         }
-
         for(int i = 0; i < nums1.length; i++) {
             nums1[i] = map.getOrDefault(nums1[i], -1);
         }
-
-
         return nums1;
     }
 
     public static void main(String[] args) {
-
         Solution s = new Solution();
-
         System.out.println(Arrays.toString(s.nextGreaterElement(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2})));
-
     }
 
 }
