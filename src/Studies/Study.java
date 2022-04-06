ae6009a2f116cd25eff41aee3bbddc5703641d58
leodots <2212.leonardo@gmail.com>
2022-04-06 15:34:18 -0300

    Studying google challenges.

--- src/Studies/Study.java
+++ src/Studies/Study.java
@@ -3,33 +3,21 @@ package Studies;
 
 public class Study {
 
+    public Study print() {
+        System.out.println("Printed");
+        return this;
+    }
 
-    public static boolean isEqualToSum(int[] array, int sum) {
-
-        int high = array.length - 1;
-        int low = 0;
-
-        while(sum(array, high, low) != sum) {
-
-            if(sum(array, high, low) < sum) low++;
-
-            if(sum(array, high, low) > sum) high--;
-
-            if(low >= array.length - 1 || high < 0) return false;
-
-            if(high == low && sum(array, high, low) != sum) return false;
-        }
+    public boolean printAgain() {
+        System.out.println("Wow.");
         return true;
     }
 
-    public static int sum(int[] array, int high, int low) {
-        return array[high] + array[low];
-    }
-
-
     public static void main(String[] args) {
 
-        System.out.println(isEqualToSum(new int[]{1, 2, 4, 6}, 10));
+        Study s = new Study();
+
+        System.out.println(s.print().print().print().print());
 
     }
 }
